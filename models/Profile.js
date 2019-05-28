58d599733add848e9bfbfd486f00eacf67537499
Tundrasama <tundrasama@gmail.com>
2019-05-28 12:11:50 -0500

    End course 16 -- added profile.js

--- models/Profile.js
+++ models/Profile.js
@@ -59,56 +59,55 @@ const ProfileSchema = new mongoose.Schema({
   ],
   education: [
     {
-        school: {
-          type: String,
-          required: true
-        },
-        degree: {
-          type: String,
-          required: true
-        },
-        fieldofstudy: {
-          type: String,
-          required = true
-        },
-        from: {
-          type: Date,
-          required: true
-        },
-        to: {
-          type: Date
-        },
-        current: {
-          type: Boolean,
-          deafult: false
-        },
-        description: {
-          type: String
-        }
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        deafult: false
+      },
+      description: {
+        type: String
       }
+    }
   ],
   social: {
-      youtube: {
-          type: String
-      },
-      twitter: {
-          type: String
-      },
-      facebook: {
-          type: String
-      },
-      linkedin: {
-          type: String
-      },
-      instagram: {
-          type: String
-      },
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
   },
   date: {
-      type: Date,
-      default: Date.now
+    type: Date,
+    default: Date.now
   }
-
 });
 
 module.exports = Profile = mongoose.model('profile', ProfileSchema);
